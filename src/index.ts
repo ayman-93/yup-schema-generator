01b89b07383175e1dcc3e363e67dfc5b2ef67ad0
ayman-93 <aymanfaisal.cs@gmail.com>
2020-01-01 22:31:43 +0300

    added custome yup to include user error message if he used setLocale

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import GenerateSchema from './GenerateSchema';
 
-export const CreateDynamicSchema = (config: [] | string) => {
-  let configArray: [];
-  if (!Array.isArray(config)) {
-    configArray = JSON.parse(config);
-  } else {
-    configArray = config;
-  }
-  return configArray.reduce(GenerateSchema, {});
+export const CreateDynamicSchema = (config: [] | string, customYup: any) => {
+    let configArray: [];
+    if (!Array.isArray(config)) {
+        configArray = JSON.parse(config);
+    } else {
+        configArray = config;
+    }
+    return configArray.reduce((schema, config) => GenerateSchema(schema, config, customYup), {});
 };
